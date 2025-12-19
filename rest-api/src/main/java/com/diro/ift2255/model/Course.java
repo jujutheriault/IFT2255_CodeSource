c3e0194b1cfb447de957004e14962237f31c01ea
vincent carbonneau <vincentcarbonneau16@gmail.com>
2025-12-19 14:08:19 -0500

    javadoc etudiant

--- rest-api/src/main/java/com/diro/ift2255/model/Course.java
+++ rest-api/src/main/java/com/diro/ift2255/model/Course.java
@@ -15,13 +15,34 @@ public class Course {
         this.name = name;
         this.description = desc;
     }
-
+    /**
+     * Pour retourner l'identifiant du cours
+     * @return l'identifiant du cours.
+     */
     public String getId() { return id; }
+    /**
+     * Setter pour l'identifiant du cours
+     * @param id l'identifiant du cours.
+     */
     public void setId(String id) { this.id = id; }
-
+    /**
+     * Getter pour le nom du cours
+     * @return le nom du cours
+     */
     public String getName() { return name; }
+    /**
+     * Setter pour le nom du cours
+     * @param name le nom du cours
+     */
     public void setName(String name) { this.name = name; }
-
+    /**
+     * Getter pour la description du cours
+     * @return
+     */
     public String getDescription() { return description; }
+    /** 
+     * Setter pour la description du cours
+     * @param email la description du cours
+     */
     public void setDescription(String email) { this.description = email; }
 }
